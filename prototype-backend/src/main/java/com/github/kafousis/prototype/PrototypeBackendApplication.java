c01d802ed78ea8b6a9d56bb8370b824a1746aa67
Giannis Kafousis <g.kafousis@gmail.com>
2022-10-17 14:58:49 +0300

    backend deploy to external tomcat conf

--- prototype-backend/src/main/java/com/github/kafousis/prototype/PrototypeBackendApplication.java
+++ prototype-backend/src/main/java/com/github/kafousis/prototype/PrototypeBackendApplication.java
@@ -2,9 +2,10 @@ package com.github.kafousis.prototype;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PrototypeBackendApplication {
+public class PrototypeBackendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrototypeBackendApplication.class, args);
